bfa2b21b45a2119c5b85308d1f87573a7f49e0d2
salonipawale <pawalesaloni123@gmail.com>
2025-07-31 17:39:27 +0530

    Jdbc with mysql

---
+++ AdvancedJava/Java JDBC/JDBC-demo/src/main/java/com/demo/DBConnection.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static void main(String[] args){
+        String url = "jdbc:mysql://localhost:3306/companydb";
+        String user = "root";
+        String password = "root@#$5";
+
+        try {
+            Connection conn = DriverManager.getConnection(url,user,password);
+            System.out.println("Connected to the database");
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            e.printStackTrace();
+        }
+    }   
+}
