2225cfd35fdc180f8af145f37acc22e22fab3d97
salonipawale <pawalesaloni123@gmail.com>
2026-03-05 12:16:33 +0530

    Updated demo-app

---
+++ java-servlet/demo-app/src/main/java/com/example/DemoServlet.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet Initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h2>Hello from Servlet</h2>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet Destroyed");
+    }
+
+    
+}
---
+++ java-servlet/demo-app/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Demo App</title>
+</head>
+<body>
+
+<h2>Welcome to Application</h2>
+
+<!-- Link to Servlet -->
+<a href="demo">Go to Servlet</a>
+
+</body>
+</html>
